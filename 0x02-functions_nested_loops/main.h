d17ff88373b5d2f7ae8ea225f7df2db22e01fe8f
0mikey <mmunene43@gmail.com>
2023-08-21 01:55:46 -0700

    0-reset_to_98.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,13 @@
-#ifndef main_h
-#define main_h
-
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
+#include "main.h"
+/**
+* main - Test function for positive or negative
+* Description: Test for positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
